ebb2f0aa6586845241eeced10404a0913ecdf081
Aaron <asubichev@gmail.com>
2022-11-30 16:12:49 -0600

    done with reading?

--- Main.java
+++ Main.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args) 
@@ -6,6 +11,29 @@ public class Main
         // create adjacency linkedlist
         // read request file
         // ^^in any order
-        
+        //args[0] - flight data
+        //args[1] - request data
+        //args[2] - output file name
+        Scanner scan = null;
+        try {
+            String ff = "test.txt";//args[0]
+            scan = new Scanner(new File(ff));
+        }
+        catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+        int numlines = scan.nextInt();
+        scan.nextLine();
+        for(int i = 0; i < numlines; i++)
+        {
+            String line = scan.nextLine();
+            String[] breh = line.split("[|]");
+            String origin = breh[0];
+            String destination = breh[1];
+            int cost = Integer.parseInt(breh[2]);
+            int time = Integer.parseInt(breh[3]);
+            
+        }
     }
 }
