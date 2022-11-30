cd0474d7dc96cc4b6a9aec602d9fdf23d23fcdb6
Aaron <asubichev@gmail.com>
2022-11-30 15:37:01 -0600

    lil to do list done

--- Main.java
+++ Main.java
@@ -2,6 +2,10 @@ public class Main
 {
     public static void main(String[] args) 
     {
+        // read data file
+        // create adjacency linkedlist
+        // read request file
+        // ^^in any order
         
     }
 }
