dfada8de0ac3552b6aec230706108c033c53ff82
asubichev <asubichev@gmail.com>
2022-12-05 17:37:23 -0600

    addPair @ ThaList

--- CityNode.java
+++ CityNode.java
@@ -1,26 +1,26 @@
-public class CityNode
-{
+public class CityNode {
+
     private String city;
     private int cost;
     private int duration;
-    private CityNode next;
+    private CityNode down;
+    private CityNode right;
 
-    public CityNode()
-    {
+    public CityNode() {
         city = "";
         cost = 0;
         duration = 0;
-        next = null;
+        down = null;
+        right = null;
     }
 
-    public CityNode(String city, int cost, int duration)
-    {
+    public CityNode(String city, int cost, int duration) {
         this.city = city;
         this.cost = cost;
         this.duration = duration;
-        next = null;
+        down = null;
+        right = null;
     }
-
 
     public String getName() {
         return this.city;
@@ -46,11 +46,24 @@ public class CityNode
         this.duration = duration;
     }
 
-    public CityNode getNext() {
-        return this.next;
+    public CityNode getDown() {
+        return this.down;
     }
 
-    public void setNext(CityNode next) {
-        this.next = next;
+    public void setDown(CityNode next) {
+        this.down = next;
+    }
+
+    public CityNode getRight() {
+        return right;
+    }
+
+    public void setRight(CityNode right) {
+        this.right = right;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return ((CityNode) o).getName().equals(this.city);
     }
 }
--- ThaList.java
+++ ThaList.java
@@ -1,28 +1,79 @@
+import java.security.InvalidParameterException;
 import java.util.LinkedList;
 
-public class ThaList
-{
+public class ThaList {
+
     private LinkedList list;
     private CityNode head;
-    
-    public ThaList()
-    {
+
+    public ThaList() {
         list = null;
     }
 
-    //overloaded constructor not needed
-    public boolean addCity(String name)
-    {
-        if (head.getName().equals(name)) return false; //if node we're adding exists in head
+    // overloaded constructor not needed
+    public boolean addCity(String name) {
+        if (head.getName().equals(name))
+            return false; // if node we're adding exists in head
         CityNode temp = head;
-        while(temp.getNext() != null)
-        {
-            if(temp.getName().equals(name)) return false;
-            temp = temp.getNext();
+        while (temp.getDown() != null) {
+            if (temp.getName().equals(name))
+                return false;
+            temp = temp.getDown();
         }
-        temp.setNext(new CityNode(name, 0, 0));
+        temp.setDown(new CityNode(name, 0, 0));
         return true;
     }
 
-    public CityNode getHead(){ return head; }
+    public boolean addPair(String name1, String name2, int cost, int duration) {
+        if (name1.equals(name2))
+            throw new IllegalArgumentException("City 1 == City 2");
+        CityNode temp = head;
+        if (!head.getName().equals(name1)) {
+            // head ain't name1
+            while (temp.getDown() != null) {
+                if (temp.getName().equals(name1)) // we've found our node to add name2 to
+                    break;
+                temp = temp.getDown();
+            }
+            // if we broke, this if statement should fail
+            if (temp.getDown() == null) {
+                temp.setDown(new CityNode(name1, 0, 0));
+                temp = temp.getDown();
+            }
+        }
+        // we're now at the source node, need to add destination
+        while (temp.getRight() != null) {
+            if (temp.getName().equals(name2))
+                return false; // name2 already existed in name1, shouldn't happen
+            temp = temp.getRight();
+        }
+        temp.setRight(new CityNode(name2, cost, duration));
+
+        // now same thing for name2
+        temp = head;
+        if (!head.getName().equals(name2)) {
+            // name2 ain't head
+            while (temp.getDown() != null) {
+                if (temp.getName().equals(name2))
+                    break;
+                temp = temp.getDown();
+            }
+            // reached node, or end w/o find
+            if (temp.getDown() == null) {
+                temp.setDown(new CityNode(name2, 0, 0));
+                temp = temp.getDown();
+            }
+        }
+        while (temp.getRight() != null) {
+            if (temp.getName().equals(name1))
+                return false;
+            temp = temp.getRight();
+        }
+        temp.setRight(new CityNode(name2, cost, duration));
+        return true;
+    }
+
+    public CityNode getHead() {
+        return head;
+    }
 }
