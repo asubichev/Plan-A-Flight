11c36a091f2ee9239d3548e48bf8e9d1409bc4de
asubichev <asubichev@gmail.com>
2022-12-08 17:49:31 -0600

    finish

--- Main.java
+++ Main.java
@@ -1,8 +1,10 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.Scanner;
-
-import javax.activity.InvalidActivityException;
 
 public class Main {
 
@@ -11,12 +13,18 @@ public class Main {
         // create adjacency linkedlist
         // read request file
         // ^^in any order
-        // args[0] - flight data
-        // args[1] - request data
-        // args[2] - output file name
+        
+        // args = new String[3];
+        // args[0] = "flightdata.txt";
+        // args[1] = "request.txt";
+        // args[2] = "output.txt";
+
+
+        // Create linked list-----------------------------------------------------------------------
+        // -----------------------------------------------------------------------------------------
         Scanner scan = null;
         try {
-            String ff = "test.txt"; // args[0]
+            String ff = args[0]; // "flightdata.txt"
             scan = new Scanner(new File(ff));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -26,17 +34,41 @@ public class Main {
         ThaList list = new ThaList();
         for (int i = 0; i < numlines; i++) {
             String line = scan.nextLine();
-            String[] breh = line.split("[|]");
+            String[] data = line.split("[|]");
             // Not sure if this is the proper exception to use
-            if (breh.length != 4)
-                throw new IllegalArgumentException("Incorrect input detected. Please check input file.");
-            list.addPair(breh[0], breh[1], Double.parseDouble(breh[2]), Integer.parseInt(breh[3]));
+            if (data.length != 4)
+                throw new IllegalArgumentException("Incorrect input detected. Please check flight data file.");
+            list.addPair(data[0], data[1], Double.parseDouble(data[2]), Integer.parseInt(data[3]));
 
         }
-        System.out.println(list);
-        String bee = list.findPath("Austin", "Houston", 1, true);
-        System.out.println(bee);
-        bee = list.findPath("Oklahoma City", "Philadelphia", 1, false);
-        System.out.println(bee);
+        // Get possible paths------------------------------------------------------------------
+        // ------------------------------------------------------------------------------------
+        try {
+            String ff = args[1]; // "request.txt"
+            scan = new Scanner(new File(ff));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        numlines = scan.nextInt();
+        scan.nextLine();
+        String toFile = "";
+        for (int i = 0; i < numlines; i++) {
+            String line = scan.nextLine();
+            String[] data = line.split("[|]");
+            if (data.length != 3)
+                throw new IllegalArgumentException("Incorrect input detected. Please check flight reqeust file.");
+            boolean flag = false;
+            if (data[2].equals("C"))
+                flag = true;
+            String toStdOut = list.findPath(data[0], data[1], i, flag);
+            toFile += toStdOut + "\n";
+        }
+        // Write paths to file-----------------------------------------------------------------
+        // ------------------------------------------------------------------------------------
+        try {
+            Files.write(Paths.get(args[2]), toFile.getBytes(), StandardOpenOption.WRITE, StandardOpenOption.CREATE);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
